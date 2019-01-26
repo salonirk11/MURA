5fdc8f7d482b45de7d4db46f39d8dcb9325e3cb5
salonirk11 <salonirk11@gmail.com>
2019-01-26 14:35:00 -0600

    refactor code

---
+++ Densenet169/Full_Data/config.py
@@ -0,0 +1,11 @@
+# CONSTANTS
+# ------------------------------------------------------------------------------
+
+DATASET_DIR = 'MURA-v1.1/'
+TRAIN_IMAGES = 'train_image_paths.csv'
+TRAIN_LABELS = 'train_labeled_studies.csv'
+
+NUM_CLASSES = 1
+NUM_SAMPLES=32000
+VAL_SIZE=3700
+BATCH_SIZE=512
